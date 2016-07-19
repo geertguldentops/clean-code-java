64b83f7c40e2a633089fb8bb1aaae893c36127c5
Geert Guldentops <geert.guldentops@ipc.be>
2016-07-19 21:55:24 +0200

    Added example code to demonstrate how parameters in functions can be reduced.

---
+++ src/main/java/be/jidoka/clean/code/functions/parameters/application/CarService.java
@@ -0,0 +1,26 @@
+package be.jidoka.clean.code.functions.parameters.application;
+
+import be.jidoka.clean.code.functions.parameters.domain.Car;
+
+import java.awt.*;
+
+public class CarService {
+
+    public Car createCar() {
+        // Extra benefits:
+        //  + Can simply leave out optional fields, no need to pass in nulls to optional parameters. (e.g.: kilometersDriven)
+        //  + Clients can provide fields in the order they want.
+        //  + Easier to see which argument belongs to which parameter. (e.g.: With a constructor, is the second the String the make or the ownerName?)
+        Car car = Car.CarBuilder.carBuilderWith()
+                .ownerName("Geert Guldentops")
+                .brand("BMW")
+                .make("116 D")
+                .color(Color.BLUE)
+                // Simply leave out any unused fields.
+//                .kilometersDriven(90_000)
+                .build();
+
+        return car;
+    }
+
+}
---
+++ src/main/java/be/jidoka/clean/code/functions/parameters/domain/Car.java
@@ -0,0 +1,80 @@
+package be.jidoka.clean.code.functions.parameters.domain;
+
+import java.awt.*;
+
+public class Car {
+
+    private String brand;
+
+    private String make;
+
+    private String ownerName;
+
+    private int kilometersDriven;
+
+    private Color color;
+
+    // Avoid having 5 parameters in this constructor (+ maintenance when it becomes more or less!)
+    // Hide constructor, client's should use the CarBuilder!
+    private Car(CarBuilder carBuilder) {
+        this.brand = carBuilder.brand;
+        this.make = carBuilder.make;
+        this.ownerName = carBuilder.ownerName;
+        this.kilometersDriven = carBuilder.kilometersDriven;
+        this.color = carBuilder.color;
+    }
+
+    // GoF Builder pattern.
+    public static class CarBuilder {
+
+        private String brand;
+
+        private String make;
+
+        private String ownerName;
+
+        private int kilometersDriven;
+
+        private Color color;
+
+        // Hide Builder constructor.
+        private CarBuilder() {
+        }
+
+        // Static factory to instantiate the Builder.
+        public static CarBuilder carBuilderWith() {
+            return new CarBuilder();
+        }
+
+        public CarBuilder brand(String brand) {
+            this.brand = brand;
+            return this;
+        }
+
+        public CarBuilder make(String make) {
+            this.make = make;
+            return this;
+        }
+
+        public CarBuilder ownerName(String ownerName) {
+            this.ownerName = ownerName;
+            return this;
+        }
+
+        public CarBuilder kilometersDriven(int kilometersDriven) {
+            this.kilometersDriven = kilometersDriven;
+            return this;
+        }
+
+        public CarBuilder color(Color color) {
+            this.color = color;
+            return this;
+        }
+
+        public Car build() {
+            return new Car(this);
+        }
+
+    }
+
+}
