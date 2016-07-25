9106104042f3b90c61936cd2d71d1f7f287ddd10
Geert Guldentops <geert.guldentops@ipc.be>
2016-07-25 21:26:37 +0200

    Remove classes used when demonstration git rebase -i

--- src/main/java/be/jidoka/clean/code/exception/handling/TestException.java
+++
@@ -1,7 +0,0 @@
-package be.jidoka.clean.code.exception.handling;
-
-/**
- * Created by geertguldentops on 25/07/16.
- */
-public class TestException {
-}
--- src/main/java/be/jidoka/clean/code/meaningful/names/encodings/doinstead/domain/Test.java
+++
@@ -1,7 +0,0 @@
-package be.jidoka.clean.code.meaningful.names.encodings.doinstead.domain;
-
-/**
- * Created by geertguldentops on 25/07/16.
- */
-public class Test {
-}
