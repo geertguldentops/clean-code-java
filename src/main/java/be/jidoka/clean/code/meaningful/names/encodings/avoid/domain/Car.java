fb9af095376ed41d55099d916796456cb5353ee7
Geert Guldentops <geertguldentops@jidoka.be>
2022-05-10 16:50:59 +0200

    Create a more realistic example for the Car hungarian encoding example

--- src/main/java/be/jidoka/clean/code/meaningful/names/encodings/avoid/domain/Car.java
+++ src/main/java/be/jidoka/clean/code/meaningful/names/encodings/avoid/domain/Car.java
@@ -14,12 +14,12 @@ public class Car {
     private final int m_kilometersDriven;
     private final Color m_color;
 
-    public Car(String m_brand, String m_make, String m_ownerName, int m_kilometersDriven, Color m_color) {
-        this.m_brand = m_brand;
-        this.m_make = m_make;
-        this.m_ownerName = m_ownerName;
-        this.m_kilometersDriven = m_kilometersDriven;
-        this.m_color = m_color;
+    public Car(String brand, String make, String ownerName, int kilometersDriven, Color color) {
+        this.m_brand = brand;
+        this.m_make = make;
+        this.m_ownerName = ownerName;
+        this.m_kilometersDriven = kilometersDriven;
+        this.m_color = color;
     }
 
     // IDE generated getters are now incorrect?! Or at least hard to read/pronounce.
